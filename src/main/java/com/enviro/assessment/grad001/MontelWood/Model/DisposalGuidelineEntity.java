a47bd507e1e2d0b68cd166febec44bb684a4e8b6
montel26 <montelwood202@gmail.com>
2025-01-18 02:58:13 +0200

    Adding entities models.

---
+++ src/main/java/com/enviro/assessment/grad001/MontelWood/Model/DisposalGuidelineEntity.java
@@ -0,0 +1,10 @@
+package com.enviro.assessment.grad001.MontelWood.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+public class DisposalGuidelineEntity {
+
+
+}
---
+++ src/main/java/com/enviro/assessment/grad001/MontelWood/Model/RecyclingTipEntity.java
@@ -0,0 +1,4 @@
+package com.enviro.assessment.grad001.MontelWood.Model;
+
+public class RecyclingTipEntity {
+}
