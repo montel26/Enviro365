33c2fdab64462a18606bfc1895a21e7a7dd383f6
montel26 <montelwood202@gmail.com>
2025-01-18 03:30:12 +0200

    Refactoring the service layer.

---
+++ src/main/java/com/enviro/assessment/grad001/MontelWood/Repository/DisposalGuidelineRepository.java
@@ -0,0 +1,7 @@
+package com.enviro.assessment.grad001.MontelWood.Repository;
+
+import com.enviro.assessment.grad001.MontelWood.Model.DisposalGuidelineEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DisposalGuidelineRepository extends JpaRepository<DisposalGuidelineEntity,Long> {
+}
